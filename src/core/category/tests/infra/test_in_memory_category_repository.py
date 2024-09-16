83e281fd3e8340c6634e6f21352d0890bde30fea
andredotpy <andre.gms94@gmail.com>
2024-09-16 07:52:50 -0300

    deleting category

--- src/core/category/tests/infra/test_in_memory_category_repository.py
+++ src/core/category/tests/infra/test_in_memory_category_repository.py
@@ -11,3 +11,24 @@ class TestInMemoryCategoryRepository:
 
         assert len(repository.categories) == 1
         assert repository.categories[0] == category
+
+    def test_can_get_category_by_id_in_memory(self):
+        category_filme = Category(name='Filme', description='Descrição para filmes')
+        category_serie = Category(name='Série', description='Descrição para séries')
+        repository = InMemoryCategoryRepository(categories=[category_filme, category_serie])
+
+        category = repository.get_by_id(id=category_filme.id)
+
+        assert category is not None
+        assert category == category_filme
+
+    def test_can_delete_category_in_memory(self):
+        category_filme = Category(name='Filme', description='Descrição para filmes')
+        category_serie = Category(name='Série', description='Descrição para séries')
+        repository = InMemoryCategoryRepository(categories=[category_filme, category_serie])
+
+        assert len(repository.categories) == 2
+        category = repository.delete(id=category_filme.id)
+
+        assert category is None
+        assert len(repository.categories) == 1
